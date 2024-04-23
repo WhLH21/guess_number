12f630fc666590f3e600212a021e8802c50b5c16
Dinislam <borlakov.den@mail.ru>
2024-04-23 13:57:54 +0300

    Add comments

--- guess_number.py
+++ guess_number.py
@@ -2,12 +2,13 @@
 
 from random import randint as rd
 
-guess_number = rd(1, 20)
+guess_number = rd(1, 20)  # Подбираем случайное число
 
 while True:
-
+    # Здесь хранится ответ пользователя
     user_input = int(input("Enter the number: "))
-    
+
+    # Здесь прописана логика игры
     if user_input == guess_number:
         print("Its guess number!")
         break
@@ -15,4 +16,3 @@ while True:
         print("Guess number is less")
     else:
         print("Guess nubmer is greater")
-        
